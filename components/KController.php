3b6461a4ef5777822672db83c70f2cbc711a9625
Denis Komlev <heavytalks@gmail.com>
2014-01-23 15:13:17 +0300

    File renamed and added resource queue by priority

--- components/KController.php
+++ components/KController.php
@@ -10,13 +10,16 @@ class Controller extends CController
     private $_assetsUrl;
     private $_jsConfig = array();
     private $_data = array();  // global data needed to be availiable in nested views
+    private $_cssFilesQueue = array();
+    private $_scriptFilesQueue = array();
 
     public function init()
     {
         Yii::app()->clientScript->registerCoreScript('jquery');
-        Yii::app()->clientScript->registerCssFile($this->getAssetsUrl() . '/bootstrap/css/bootstrap.min.css');
-        Yii::app()->clientScript->registerScriptFile($this->getAssetsUrl() . '/bootstrap/js/bootstrap.min.js', CClientScript::POS_END);
-        Yii::app()->clientScript->registerCssFile($this->getAssetsUrl() . '/css/main.css');
+        $this->enqueueCssFile(1, $this->getAssetsUrl() . '/libraries/bootstrap/css/bootstrap.min.css');
+        $this->enqueueScriptFile(1, $this->getAssetsUrl() . '/libraries/bootstrap/js/bootstrap.min.js', CClientScript::POS_END);
+        $this->enqueueCssFile(100, $this->getAssetsUrl() . '/css/main.css');
+        $this->enqueueScriptFile(100, $this->getAssetsUrl() . '/js/main.js', CClientScript::POS_END);
 
         $this->setJsConfig(array(
             'baseUrl' => Yii::app()->baseUrl,
@@ -26,6 +29,7 @@ class Controller extends CController
 
     protected function beforeRender($view)
     {
+        $this->registerEnqueuedResources();
         $this->registerJsConfig();
         return true;
     }
@@ -71,9 +75,58 @@ class Controller extends CController
         }
     }
 
+    public function enqueueCssFile($priority, $url, $media = '')
+    {
+        $this->_cssFilesQueue[] = array(
+            'priority' => $priority,
+            'url' => $url,
+            'media' => $media
+        );
+    }
+
+    public function enqueueScriptFile($priority, $url, $position = null, $htmlOptions = array())
+    {
+        $this->_scriptFilesQueue[] = array(
+            'priority' => $priority,
+            'url' => $url,
+            'position' => $position,
+            'htmlOptions' => $htmlOptions
+        );
+    }
+
+    public function dequeueCssFile($url)
+    {
+        foreach ($this->_cssFilesQueue as $key => $item) {
+            if ($url == $item['url']) unset($this->_cssFilesQueue[$key]);
+        }
+    }
+
+    public function dequeueScriptFile($url)
+    {
+        foreach ($this->_scriptFilesQueue as $key => $item) {
+            if ($url == $item['url']) unset($this->_scriptFilesQueue[$key]);
+        }
+    }
+
     protected function registerJsConfig()
     {
         $js = 'var config=' . json_encode($this->_jsConfig) . ';';
         Yii::app()->clientScript->registerScript('config', $js, CClientScript::POS_HEAD);
+    }
+
+    protected function registerEnqueuedResources()
+    {
+        if (!empty($this->_cssFilesQueue)) {
+            $queue = KArrayHelper::sortTable($this->_cssFilesQueue, 'priority');
+            foreach ($queue as $item) {
+                Yii::app()->clientScript->registerCssFile($item['url'], $item['media']);
+            }
+        }
+        if (!empty($this->_scriptFilesQueue)) {
+            $queue = KArrayHelper::sortTable($this->_scriptFilesQueue, 'priority');
+            foreach ($queue as $item) {
+                Yii::app()->clientScript->registerScriptFile($item['url'], $item['position'], $item['htmlOptions']);
+            }
+        }
     }
 }
